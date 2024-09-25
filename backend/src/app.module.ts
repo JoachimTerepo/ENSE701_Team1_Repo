f3e50fc81edac1ebe9a035e07a38fa6c88cd7c77
TheRangiCrew <ryan.z.adam@gmail.com>
2024-09-26 10:31:32 +1200

    Add the user module to the app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { UserModule } from './api/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI)
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
