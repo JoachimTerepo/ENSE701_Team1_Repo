cbb57a0491873e7aa6c1c85d2466b58f4cecf273
TheRangiCrew <ryan.z.adam@gmail.com>
2024-09-27 10:01:08 +1200

    Added create method

--- backend/src/api/user/user.controller.ts
+++ backend/src/api/user/user.controller.ts
@@ -1,10 +1,21 @@
 
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './create-user-dto';
 
 @Controller('/api/user')
 export class UserController {
     @Get()
     findAll(): string {
         return 'This action returns all cats';
+    }
+
+    @Post('/create')
+    async create(@Body() createUserDto: CreateUserDto) {
+        try {
+            await this.create(createUserDto)
+            return "successful"
+        } catch (e) {
+            return "Something went wrong\n" + e
+        }
     }
 }
