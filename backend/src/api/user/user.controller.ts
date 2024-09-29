6da174bbd2117e76aedc1947d57aa295c9b914ab
TheRangiCrew <ryan.z.adam@gmail.com>
2024-09-30 10:07:20 +1300

    Creating a user works now

--- backend/src/api/user/user.controller.ts
+++ backend/src/api/user/user.controller.ts
@@ -1,18 +1,21 @@
 
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './create-user-dto';
+import { UserService } from './user.service';
 
 @Controller('/api/user')
 export class UserController {
+    constructor(private readonly userService: UserService) { }
+
     @Get()
     findAll(): string {
         return 'This action returns all cats';
     }
 
     @Post('/create')
-    async create(@Body() createUserDto: CreateUserDto) {
+    async createUser(@Body() createUserDto: CreateUserDto) {
         try {
-            await this.create(createUserDto)
+            await this.userService.create(createUserDto)
             return "successful"
         } catch (e) {
             return "Something went wrong\n" + e
