3b5ec8e554d32315aa703367a4a0abac34879c90
TheRangiCrew <ryan.z.adam@gmail.com>
2024-10-04 08:51:54 +1300

    Added id prop

--- backend/src/api/methods/method.schema.ts
+++ backend/src/api/methods/method.schema.ts
@@ -1,11 +1,13 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
+import { HydratedDocument, Types } from "mongoose";
 
 
 export type MethodDocument = HydratedDocument<Method>
 
 @Schema({ timestamps: true })
 export class Method {
+    @Prop({ type: { type: Types.ObjectId } })
+    _id: string
     @Prop({ required: true })
     name: string
 }
