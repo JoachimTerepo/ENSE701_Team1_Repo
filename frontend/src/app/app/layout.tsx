421596d24ef81993034b1a3fd9d7f59b59f1bde8
TheRangiCrew <ryan.z.adam@gmail.com>
2024-10-09 13:05:37 +1300

    Fixing navigation including adding a navbar

---
+++ frontend/src/app/app/layout.tsx
@@ -0,0 +1,18 @@
+import NavLink from "../lib/components/NavLink";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div className="w-100 border-b border-neutral-400 shadow flex justify-between items-center">
+        <div className="w-1/2 flex justify-start items-center px-2">
+          <h1 className="text-xl font-bold py-4">SPEED</h1>
+        </div>
+        <div className="w-1/2 flex justify-end items-center px-2">
+          <NavLink href="/app">Home</NavLink>
+          <NavLink href="/app/submission">Submit New Entry</NavLink>
+        </div>
+      </div>
+      <div className="w-100 h-dvh">{children}</div>
+    </>
+  );
+}
