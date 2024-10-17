d034590edf359d66d8c8be221e1fb65abc3bbe1f
JoachimTerepo <joachimterepo6@gmail.com>
2024-10-17 19:20:42 +1300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors();  // Enable CORS if needed
+  await app.init();  // Initialize the app, but don't call `listen()`
 }
-bootstrap();
+
+// Export the initialized app for Vercel
+export const app = bootstrap();
