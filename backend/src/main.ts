0908e39f7bcbc513202b9563499f397a327c201c
JoachimTerepo <joachimterepo6@gmail.com>
2024-10-17 19:56:45 +1300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const frontendUrl = 'https://ense-701-team1-repo-git-backendchanges-joachimterepos-projects.vercel.app';
+
+  // Enable CORS with specific frontend origin
   app.enableCors({
-    origin: 'https://ense-701-team1-repo-git-backendchanges-joachimterepos-projects.vercel.app', // Frontend URL
-    credentials: true, // If you need to allow credentials (cookies, auth headers)
+    origin: frontendUrl,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,  // Allow credentials like cookies, auth headers
+    allowedHeaders: 'Content-Type, Authorization',  // Add other headers if needed
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
   await app.listen(process.env.PORT || 3000);
 }
