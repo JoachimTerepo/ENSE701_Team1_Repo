0e33bf11c54c26f5c1f572a00a13ee2d8112b6a5
JoachimTerepo <joachimterepo6@gmail.com>
2024-10-17 16:52:34 +1300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://ense-701-team1-repo-git-vercel-joachimterepos-projects.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
