f9ad9d43d108ba7cff55c7145cc9cb74b2d4f042
JoachimTerepo <joachimterepo6@gmail.com>
2024-10-17 21:36:36 +1300

    update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 // run backend command "npm run start"
 // run frontend command "npm run dev"
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   // Enable CORS
   app.enableCors({ 
@@ -12,7 +15,7 @@ async function bootstrap() {
     credentials: true 
   });
 
-  const port = process.env.PORT || 8082;
+  const port = process.env.PORT || 3000;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
 
